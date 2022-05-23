d62f6ec0d1e2c8d052863caf53ea99402bb8fd6b
unknown <edwardachike@gmail.com>
2022-05-23 15:26:54 -0700

    fixing linter error -3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import Book from './Book';
+import Book from './Book.js';
+
 window.addEventListener('DOMContentLoaded', () => {
   const bookSection = document.querySelector('.book-list');
   const bookTitle = document.getElementById('title');
